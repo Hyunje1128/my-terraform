f5a7758a44b11a98c98b1d7eae0a7f1adb5172b8
Hyunje1128 <guswp1128@gmail.com>
2025-03-26 22:13:59 +0900

    ci/cd t6

--- app/scripts/start.sh
+++ app/scripts/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 APP_DIR="/home/ec2-user/app"
-JAR_NAME=$(ls $APP_DIR/*.jar | head -n 1)
+LOG_FILE="$APP_DIR/app.log"
 
-echo "[INFO] Starting application..."
+echo "[INFO] Starting static web serveron port 80..." | tee -a "$LOG_FILE"
 
+# Python을 이용한 간단한 정적 서버 실행 (port 80)
 cd $APP_DIR
 
 # 백그라운드 실행
-nohup java -jar $JAR_NAME > app.log 2>&1 &
-echo "[INFO] Application started with PID $!" >> app.log
+nohup python3 -m http.server 80 > "$LOG_FILE" 2>&1 &
+echo "[INFO] Static server started on port 8080" >> "$LOG_FILE"
